cab9661b2da406e9238731f3a5647dac6f4df357
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-15 15:29:01 -0500

    Feat (Add) Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+# Create a file.
+
+file { '/tmp/school':
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
