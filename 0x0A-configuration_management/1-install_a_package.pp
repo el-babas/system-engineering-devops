b1b4478d0c617af76646f71c9def68b667d85cac
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-15 15:49:22 -0500

    Feat (Add) Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 # Install a package - Using Puppet, install puppet-lint.
 
 package { 'puppet-lint':
+  name     => 'puppet-lint',
   ensure   => '2.5.0',
   provider => 'gem',
+  source   => 'http://rubygems.org',
 }
