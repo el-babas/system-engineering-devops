46e0ff50ca55bd6bd9b6d4b03929d4ce831a068e
cristhian1107 <cristhian.cjaa@gmail.com>
2022-04-19 20:59:51 -0500

    Feat (Add) Puppet configuration

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,12 @@
+# Install Nginx web server (All previous puppet)
+
+exec {'install':
+  provider => shell,
+  command  => 'sudo apt-get -y update ;\
+                sudo apt-get -y install nginx ;\
+                sudo chown -R ubuntu /var/www ;\
+                echo "Hello World" | sudo tee /var/www/html/index.nginx-debian.html ;\
+                sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/cristhian1107 permanent;/" \
+                    /etc/nginx/sites-available/default ;\
+                sudo service nginx start',
+}
