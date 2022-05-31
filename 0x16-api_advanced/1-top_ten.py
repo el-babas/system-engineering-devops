fd0afaeb7388cf0e85c76a7e5a1b046de3074f96
cristhian1107 <cristhian.cjaa@gmail.com>
2022-05-31 16:59:51 -0500

    Feat (Add) Top ten!

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Top Ten!
+Functions:
+    number_of_subscribers
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (string): Reddit subscriber.
+    """
+    api_header = {
+        'User-Agent': 'Mozilla/5.0'
+    }
+    api_url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    api_res = requests.get(api_url, headers=api_header, allow_redirects=False)
+    if api_res.status_code != 200:
+        print(None)
+    api_data = (api_res.json()).get('data')
+    for children in api_data.get('children'):
+         print((children.get('data')).get('title'))
